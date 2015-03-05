dda3c35cfd66ab3311c334adefec4feb022a518e
MalloryEaton <meaton@harding.edu>
2015-03-04 22:39:57 -0600

    added burt and waddleDee classes

---
+++ Platformer/Platformer/Platformer/WaddleDee.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+    class Burt
+    {
+        // Texture stuff
+        Point frameSize = new Point(46, 40);
+        Point currentFrame = new Point(0, 0);
+        Point sheetSize = new Point(8, 1);
+
+        // Framerate stuff
+        int timeSinceLastFrame = 0;
+        int millisecondsPerFrame = 200;
+    }
+}
