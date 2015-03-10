97afe9888ac269311855e85c0baf0b7fcaa0f6b7
Sam Hipp <samhipp93@gmail.com>
2015-03-09 19:57:50 -0500

    Started working on the enemies.  Didn't think you'd be editing the WaddleDee class, so I added to it.  Changing the csproj class happens whenever we add a file, fixing any conflicts from that should be easy, just keep both versions and remove the stuff Git adds.  waddleDee_test has the animation facing the other way, but I just found out you can flip a sprite using SpriteEffects, don't know how that works yet, though.

---
+++ Platformer/Platformer/Platformer/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Platformer
+{
+    class Enemy
+    {
+        public Body Body { get; protected set; }
+        public Vector2 Origin { get; set; }
+        protected Texture2D Texture { get; set; }
+    }
+}
--- Platformer/Platformer/Platformer/WaddleDee.cs
+++ Platformer/Platformer/Platformer/WaddleDee.cs
@@ -1,21 +1,56 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Platformer
 {
-    class WaddleDee
+    class WaddleDee : Enemy
     {
+        
         // Texture stuff
         Point frameSize = new Point(46, 40);
         Point currentFrame = new Point(0, 0);
         Point sheetSize = new Point(8, 1);
-
+        
         // Framerate stuff
         int timeSinceLastFrame = 0;
         int millisecondsPerFrame = 200;
+
+        public WaddleDee(World world, Texture2D texture, Vector2 position)
+        {
+            Texture = texture;
+            Body = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(frameSize.X / 2), ConvertUnits.ToSimUnits(frameSize.Y / 2), 1f, position);
+            //Origin = new Vector2(ConvertUnits.ToSimUnits(frameSize.X / 2),
+            //    ConvertUnits.ToSimUnits(frameSize.Y / 2));
+            Origin = new Vector2(0f, 0f);
+        }
+
+        public void MoveRight()
+        {
+            //TODO: adjust spritesheet to make him appear to move right. Actual movement will be done in the Game class.
+        }
+
+        public void MoveLeft()
+        {
+            //TODO: adjust spritesheet to make him appear to move left.  Actual movement will be done in the Game class.
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(Texture,
+                ConvertUnits.ToDisplayUnits(Body.Position),
+                new Rectangle(0, 0, frameSize.X, frameSize.Y),
+                Color.White,
+                0f,
+                Origin,
+                1f,
+                SpriteEffects.None,
+                0f);
+        }
+
+
     }
 }
